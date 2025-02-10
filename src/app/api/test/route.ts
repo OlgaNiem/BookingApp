db97301a0a67eaeba33bbfd0f9bcffb216b8b266
OlgaNiem <olhaniemiets@gmail.com>
2025-02-10 10:56:52 +0200

    new test

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/prisma";
+
+export async function GET(req: Request) {
+  try {
+    const users = await db.user.findMany({
+      select: { email: true, name: true },
+    });
+
+    return new Response(JSON.stringify(users), {
+      status: 200, 
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return new Response(
+      JSON.stringify({ error: "Failed to fetch data" }),
+      { status: 500 }
+    );
+  }
+}
